3f29b203f7e80cb5809f02a4a3efb3192a882a00
ElphaTech <fabio.bernasconi8@gmail.com>
2026-03-05 22:18:34 +1300

    Allowed access from main site

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "https://sammyhost.uk",
+    "https://www.sammyhost.uk",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET"], # You can be specific here for better security
+    allow_headers=["*"],
+)
 
 @app.get("/ping")
 def ping():
